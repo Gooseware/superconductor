1d892e1218e8d0cfdf361235243116c1feadc5f3
Paul <goosewares@gmail.com>
2026-07-27 18:02:45 +0400

    chore: Remove scratch test files left by processor

--- test_dispatcher.ts
+++
@@ -1,9 +0,0 @@
-import { ParallelDispatcher } from './packages/engine/src/dispatcher/parallel-dispatcher.ts';
-
-function testBoundary() {
-    const disp0 = new ParallelDispatcher(0);
-    console.log("N=0", disp0['maxConcurrent']);
-    const disp1 = new ParallelDispatcher(1);
-    console.log("N=1", disp1['maxConcurrent']);
-}
-testBoundary();
