0a3c2daae1aa3e56d86d4fad9cf280bb6539c298
Paul <goosewares@gmail.com>
2026-08-01 12:32:13 +0400

    chore: remove scratch test files and node_modules from index

--- test_glob.js
+++
@@ -1,3 +0,0 @@
-const path = require('path');
-console.log("1:", path.matchesGlob('../packages/engine/src/index.ts', 'packages/*/src/**'));
-console.log("2:", path.matchesGlob('packages/engine/src/index.ts', 'packages/*/src/**'));
